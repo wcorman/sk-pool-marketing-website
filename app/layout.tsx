f7b83f55ce11d87df860bceafa3f12521e37615b
Wes Corman <wcorman@gmail.com>
2025-11-29 22:37:36 -0600

    Add Google Analytics tracking tag (G-WBT3099S0K)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
@@ -57,6 +58,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-br from-blue-50 to-white`}
       >
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WBT3099S0K"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WBT3099S0K');
+          `}
+        </Script>
         <header className="sticky top-0 z-50 pt-4 pb-2 px-4 backdrop-blur-md bg-transparent">
           <Navbar />
         </header>
